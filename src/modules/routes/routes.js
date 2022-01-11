0556b2ea9176058a4ed92c8137418acb453418eb
ebondarenko-exceedteam <ebondarenko.exceedteam@gmail.com>
2022-01-11 10:47:01 +0300

    create database, create scheme for users and registration

---
+++ src/modules/routes/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createNewUser,
+  authorizationUser
+} = require('../controllers/users.controller');
+
+// const {
+//   getAllRegistrations,
+//   createNewRegistration,
+//   changeRegistration,
+//   deleteRegistration
+// } = require('../controllers/registration.controller');
+
+//Users routes
+router.post('/createNewUser', createNewUser);
+// router.get('/authorizationUser', authorizationUser);
+
+//Registration routes
+// router.get('/getAllRegistrations', getAllRegistrations);
+// router.post('/createNewRegistration', createNewRegistration);
+// router.patch('/changeRegistration', changeRegistration);
+// router.delete('/deleteRegistration', deleteRegistration);
+
+module.exports = router;
