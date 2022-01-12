b62412a73ffc641f2fa80204434309c3cfc93ec0
ebondarenko-exceedteam <ebondarenko.exceedteam@gmail.com>
2022-01-12 09:48:02 +0300

    fix bugs

--- src/modules/controllers/users.controller.js
+++ src/modules/controllers/users.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {secret} = require('./config');
+const { secret } = require('./config');
 const Users = require('../../db/models/users/index');
 
 const generateAccessToken = (id) => {
@@ -9,7 +9,7 @@ const generateAccessToken = (id) => {
 }
 
 module.exports.createNewUser = async (req, res, next) => {
-  const {login, password} = req.body;
+  const { login, password } = req.body;
   const candidate = await Users.findOne({login});
   if (candidate) {
     return res.status(401).json({data: 'Login already exists'});
@@ -17,13 +17,10 @@ module.exports.createNewUser = async (req, res, next) => {
   const hashPassword = bcrypt.hashSync(password, 5);
   const users = new Users({login, password: hashPassword});
   users.save().then(result => {
-    Users.findOne({login}).then(result => {
-      const { _id, login } = result;
-      const token = generateAccessToken(_id);
-      res.send({token, login})
-    })
+    const { _id, login } = result;
+    const token = generateAccessToken(_id);
+    res.send({token, login});
   }).catch(err => {
-    console.log(err);
-    res.status(400).json({data: 'Registration error'})
+    res.status(400).json({data: 'Registration error'});
   });
 };
